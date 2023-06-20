152a60b2f2dc04d2a17646056d17c993b60eceb6
Elian Santiago <eliansanti317@gmail.com>
2023-06-20 10:29:21 -0600

    User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ContactSeeder::class,
-            RoleSeeder::class
+            RoleSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
