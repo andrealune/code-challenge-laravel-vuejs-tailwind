1c28dfdf7676930e8d98670f79f6c26a84ea86d0
Elian Santiago <eliansanti317@gmail.com>
2023-06-20 10:16:01 -0600

    Form Request to validate data

---
+++ app/Http/Requests/CreateContactRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name'  => 'required|string',
+            'phone'      => 'sometimes|string|max:10',
+            'email'      => 'sometimes|string|email|unique:contacts'
+        ];
+    }
+}
